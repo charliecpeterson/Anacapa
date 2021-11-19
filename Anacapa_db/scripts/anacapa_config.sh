c0cc5aeb3fe6085814a06d1aff91694c5d3d7831
charlie <charlie@linuxbox.home>
2021-11-19 11:50:59 -0800

    modify config.sh

--- Anacapa_db/scripts/anacapa_config.sh
+++ Anacapa_db/scripts/anacapa_config.sh
@@ -26,10 +26,8 @@ CUTADAPT="cutadapt" 		#version 1.16 path to cutadapt binary. See documentation f
 
 #load anaconda/python2-4.2
 #ANACONDA_PYTHON="module load anaconda/python2-4.2"				#or whatever code is used to load anaconda/python2-4.2 in a bash shell, or path to anaconda/python2-4.2
-ANACONDA_PYTHON=python
 #load bowtie2
 #BOWTIE2="module load bowtie2"							# version 2.3.4 or what ever code is used to load bowtie2 in a bash shell, or path to bowtie2
-BOWTIE2=`bowtie2`
 #load ATS
 #ATS="module load ATS"									#or what ever code is used to load ATS in a bash shell, or path to ATS.  ATS is a Hoffman2 module that allows the user to submit a job on the HPC from within a shell script
 
